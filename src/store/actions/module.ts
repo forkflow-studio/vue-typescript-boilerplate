e5dcb6361a11c62f66fe6edec7e785929bc40fed
Charles Spruce <karol.sw3@gmail.com>
2020-02-16 17:31:46 +0100

    fix: module.ts

--- src/store/actions/module.ts
+++ src/store/actions/module.ts
@@ -2,7 +2,7 @@ import { ActionTree } from 'vuex'
 import { ModuleState } from '@/types/module'
 import RootState from '@/types/root-state'
 
-const chatterActions: ActionTree<ModuleState, RootState> = {
+const moduleActions: ActionTree<ModuleState, RootState> = {
 }
 
-export default chatterActions
+export default moduleActions
